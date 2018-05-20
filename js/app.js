72141f5f941eca9bc93f3e6f674f188abf50550d
Faithful-Mind <lpyy15@gmail.com>
2018-05-20 22:26:54 +0800

    Rewrite in MVP pattern

--- js/app.js
+++ js/app.js
@@ -1,84 +1,133 @@
-/* STUDENTS IGNORE THIS FUNCTION
- * All this does is create an initial
- * attendance record if one is not found
- * within localStorage.
- */
-(function() {
-    if (!localStorage.attendance) {
-        console.log('Creating attendance records...');
-        function getRandom() {
-            return (Math.random() >= 0.5);
+var rawModel = {
+    _data: JSON.parse(localStorage.udacitySchoolAttendance),
+
+    refreshData() {
+        this._data = JSON.parse(localStorage.udacitySchoolAttendance);
+    },
+
+    getAllData() {
+        return this._data;
+    },
+
+    countMissing(studentName, rowIndex=null) {
+        if (rowIndex !== null) { // just `if (rowIndex) {` won't work since rowIndex starts form 0
+            return this._data[rowIndex].dailyAttendency.reduce((a, b) => a + b);
         }
-
-        var nameColumns = $('tbody .name-col'),
-            attendance = {};
-
-        nameColumns.each(function() {
-            var name = this.innerText;
-            attendance[name] = [];
-
-            for (var i = 0; i <= 11; i++) {
-                attendance[name].push(getRandom());
+        for (var i = 0; i < this._data.length; i++) {
+            if (this._data[i].name === studentName) {
+                return this._data[i].dailyAttendency.reduce((a, b) => a + b);
             }
-        });
+        }
+    },
 
-        localStorage.attendance = JSON.stringify(attendance);
-    }
-}());
-
-
-/* STUDENT APPLICATION */
-$(function() {
-    var attendance = JSON.parse(localStorage.attendance),
-        $allMissed = $('tbody .missed-col'),
-        $allCheckboxes = $('tbody input');
-
-    // Count a student's missed days
-    function countMissing() {
-        $allMissed.each(function() {
-            var studentRow = $(this).parent('tr'),
-                dayChecks = $(studentRow).children('td').children('input'),
-                numMissed = 0;
-
-            dayChecks.each(function() {
-                if (!$(this).prop('checked')) {
-                    numMissed++;
+    changeAttendancy(studentName, isAttendended, nthDay, nthStudent=null) {
+        if (nthStudent !== null) {
+            this._data[nthStudent].dailyAttendency[nthDay] = isAttendended;
+        } else {
+            for (var i = 0; i < this._data.length; i++) {
+                if (this._data[i].name === studentName) {
+                    this._data[i].dailyAttendency[nthDay] = isAttendended;
                 }
-            });
-
-            $(this).text(numMissed);
-        });
+            }
+        }
     }
+};
 
-    // Check boxes, based on attendace records
-    $.each(attendance, function(name, days) {
-        var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
-            dayChecks = $(studentRow).children('.attend-col').children('input');
+/**
+ * Proxy of rawModel for automatically saving and refreshing on invoking.
+ */
+var model = new Proxy(rawModel, {
+    get: function (target, name, receiver) {
+        target.refreshData();
+        var result = target[name];
+        if (localStorage.udacitySchoolAttendance = JSON.stringify(target._data)) {
+            // console.warn('successfully saved');
+        }
+        if(target.hasOwnProperty(name) && typeof result == 'function') {
+            // console.warn('invoking method of rawModel');
+            return new Proxy(target[name], this);
+        }
+        // console.warn('property of rawModel invoked');
+        return result;
+    },
 
-        dayChecks.each(function(i) {
-            $(this).prop('checked', days[i]);
-        });
-    });
+    apply(target, object, args) {
+        rawModel.refreshData(); // target is a function
+        var result = target.call(object, ...args);
+        // console.warn('method of rawModel invoked');
+        if (localStorage.udacitySchoolAttendance = JSON.stringify(rawModel._data)) {
+            // console.warn('successfully saved');
+        }
+        return result;
+    }
+});
 
-    // When a checkbox is clicked, update localStorage
-    $allCheckboxes.on('click', function() {
-        var studentRows = $('tbody .student'),
-            newAttendance = {};
+var presenter = {
 
-        studentRows.each(function() {
-            var name = $(this).children('.name-col').text(),
-                $allCheckboxes = $(this).children('td').children('input');
+    init() {
+        view.init();
+    },
 
-            newAttendance[name] = [];
+    getAllData() {
+        return model.getAllData();
+    },
 
-            $allCheckboxes.each(function() {
-                newAttendance[name].push($(this).prop('checked'));
+    countMissing(studentName, rowIndex=null) {
+        return model.countMissing(studentName, rowIndex);
+    },
+
+    changeAttendancy(studentName, isAttendended, nthDay, nthStudent=null) {
+        model.changeAttendancy(studentName, isAttendended, nthDay, nthStudent);
+        view.render();
+    }
+}
+
+var view = {
+    init() {
+        this._buildRows();
+    },
+
+    _buildRows() {
+        var tbody = document.querySelector('table tbody');
+        tbody.innerHTML = '';
+
+        presenter.getAllData().forEach((rowData, rowIndex) => {
+            tbody.insertAdjacentHTML('beforeend',
+                `<tr class="student">
+                    <td class="name-col">${rowData.name}</td>
+                    <td class="missed-col">${presenter.countMissing(rowData.name, rowIndex)}</td>
+                </tr>`
+            );
+            rowData.dailyAttendency.forEach((isAttended, colIndex) => {
+                var chkbox = document.createElement('input');
+                chkbox.id = `day-r${rowIndex + 1}-d${colIndex + 1}`;
+                chkbox.type = 'checkbox';
+                chkbox.checked = isAttended;
+                chkbox.addEventListener('change', () => {
+                    var isChecked = document.getElementById(`day-r${rowIndex + 1}-d${colIndex + 1}`).checked;
+                    // console.warn(isChecked);
+                    presenter.changeAttendancy(rowData.name, isChecked, colIndex, rowIndex);
+                });
+
+                var td = document.createElement('td');
+                td.appendChild(chkbox);
+
+                var tr = tbody.querySelector(`tr:nth-child(${rowIndex + 1})`);
+                tr.insertBefore(td, tbody.querySelector(`tr:nth-child(${rowIndex + 1}) td.missed-col`));
             });
         });
+    },
 
-        countMissing();
-        localStorage.attendance = JSON.stringify(newAttendance);
-    });
+    render() {
+        presenter.getAllData().forEach((rowData, rowIndex) => {
+            var tds = document.querySelectorAll(`body > table > tbody > tr:nth-child(${rowIndex + 1}) > td`);
+            var td = tds.item(tds.length - 1);
+            td.innerText = presenter.countMissing(rowData.name, rowIndex);
+            rowData.dailyAttendency.forEach((isAttended, colIndex) => {
+                var chked = document.getElementById(`day-r${rowIndex + 1}-d${colIndex + 1}`).checked = isAttended;
+            });
+        });
+    },
+};
 
-    countMissing();
-}());
+presenter.init();
